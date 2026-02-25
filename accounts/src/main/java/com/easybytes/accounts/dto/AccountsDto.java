d9a38df397f2710b21231c87269d3f743bdfdd3a
Shen Tang <stang311@gatech.edu>
2026-02-25 12:10:13 -0500

    Added a Spring Boot project with H2 Database, Spring Data JPA entities, repositories, dtos, controllers, and data source configurations with application.yaml

---
+++ accounts/src/main/java/com/easybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,9 @@
+package com.easybytes.accounts.dto;
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+    private String accountNumber;
+    private String accountType;
+    private String branchAddress;
+}
